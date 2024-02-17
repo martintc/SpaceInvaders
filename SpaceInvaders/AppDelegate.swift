4af34f3296a65a4210ba0d3d4c6dbad2eb84a675
Todd Martin <kj4ntv@gmail.com>
2024-02-16 20:18:44 -0800

    Initial Commit

---
+++ SpaceInvaders/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpaceInvaders
+//
+//  Created by Todd Martin on 2/16/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
