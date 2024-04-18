3733bf763ba489247f02d9f3db615f58bc0476bd
Todd Martin <kj4ntv@gmail.com>
2024-04-18 15:53:48 -0700

    catch up with scene arch

--- SpaceInvaders/Levels/LevelOne.swift
+++ SpaceInvaders/Levels/LevelOne.swift
@@ -23,18 +23,23 @@ class LevelOne: LevelProtocol {
         let playerNode = Player(name: "player")
         playerNode.scale = simd_float3(0.25, 0.25, 0.25)
         playerNode.position = simd_float3(0, -2, 0)
+        
+        let alienNode = Alien(name: "alien")
+        alienNode.scale = simd_float3(0.25, 0.25, 0.25)
+        
         gameObjects.append(playerNode)
+        gameObjects.append(alienNode)
     }
     
     func render(renderEncoder: any MTLRenderCommandEncoder) {
         for gameObject in gameObjects {
-            gameObject.render(renderEncoder, camera: self.camera)
+            gameObject.renderRecursive(renderEncoder, camera: self.camera)
         }
     }
     
     func update() {
         for gameObject in gameObjects {
-            gameObject.update()
+            gameObject.updateRecursive()
         }
     }
 }
